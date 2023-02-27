5157d1f7966c4892bfd56e1382d818148a05cbd8
ASP-J <jmigotrski@gmail.com>
2023-02-27 08:03:16 -0300

    sd

--- resources/views/loans/show.blade.php
+++ resources/views/loans/show.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 
     <div class="card" style="margin:20px;">
-        <div class="card-header">Book page</div>
+        <div class="card-header">Loan page</div>
         <div class="card-body">
             <div class="card-body">
-                <h5 class="card-title">Título : {{ $loans->book_id }}</h5>
-                <p class="card-text">Autor : {{ $loans->user_id }}</p>
+                <h5 class="card-title">id do livro : {{ $loans->book_id }}</h5>
+                <p class="card-text">id do usuario : {{ $loans->user_id }}</p>
 
             </div>
             </hr>
